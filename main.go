8183bc9e683ba43bdc9fd20a2ab19c65cc1f8a47
unknown <anudeep9704@gmail.com>
2024-08-15 08:48:10 -0500

    Added switch

--- main.go
+++ main.go
@@ -10,4 +10,17 @@ func main() {
 	a, b := 11.0, 9.0
 	fmt.Println(simplecalc.Add(a, b))
 	fmt.Println(simplecalc.Sub(a, b))
+
+	day := 2
+
+	switch day {
+	case 1, 3, 5:
+		fmt.Println("Odd weekday")
+	case 2, 4:
+		fmt.Println("Even weekday")
+	case 6, 7:
+		fmt.Println("Weekend")
+	default:
+		fmt.Println("Invalid day of day number")
+	}
 }
